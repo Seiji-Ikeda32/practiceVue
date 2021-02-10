239ed45b3433fefd38655af1ae2e554115ca66b5
池田聖示 <ikedakiyoshishimese@ikedakiyoshishimesenoMacBook-Air.local>
2021-02-10 22:43:03 +0900

    first vue

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello My first Vue!'
+    }
+});
+
+const example = new Vue({
+    el: '#example',
+    data: {
+      greeting: 'Hello Vue.js!',
+    },
+});
+
+const googleLink = new Vue({
+    el: '#googleLink',
+    data: {
+      url: 'https://www.google.com/?hl=ja',
+    },
+});
+
+// var app2 = new Vue({
+//     el: '#app-2',
+//     data: {
+//       message: 'You loaded this page on ' + new Date().toLocaleString()
+//     }
+//   })
+  
